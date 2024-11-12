899966d7679420e1e5234f7bbe9ac9f7ac133f5d
School <justin.battrick@mohawkcollege.ca>
2024-11-12 01:57:27 -0500

    Adds some logic comments for the user interaction on the ball.

--- Ball.cs
+++ Ball.cs
@@ -32,6 +32,19 @@ namespace Game10003
             Draw.Circle(400, 575,25);
         }
 
+        private void userInteraction()
+        {
+            // Check if the ball is at the bottom of the screen
+            if ()
+            {
+                // If ball is at the bottom of the screen stop its motion
+
+                // Using the mouse position draw a dotted line between the ball and the cursor location to show the path of the ball
+            } 
+            
+            // When the user clicks on the left mouse button release the ball following the path
+        }
+
         private void collisionDetection()
         {
 
